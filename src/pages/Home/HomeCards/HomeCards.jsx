6090988ceeb0503ca83c48ce8d05d6ffac0ec046
ismail <coderismail22@gmail.com>
2024-09-12 12:06:45 +0600

    complete home cards type 1

--- src/pages/Home/HomeCards/HomeCards.jsx
+++ src/pages/Home/HomeCards/HomeCards.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import HomeCard from "../../../components/HomeCard/HomeCard";
 import axios from "axios";
+import { RotatingLines } from "react-loader-spinner";
 
 const HomeCards = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [visibleBlogs, setVisibleBlogs] = useState(2);
+  // Initial 2 visible blogs
+  // use "setVisibleBlogs" if you want to implement "More Posts" button handler
+
 
   const fetchPosts = async () => {
     setLoading(true);
@@ -23,11 +28,42 @@ const HomeCards = () => {
   useEffect(() => {
     fetchPosts();
   }, []);
+
+
+  // Loading Spinner
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <RotatingLines
+          visible={true}
+          height="46"
+          width="46"
+          color="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          ariaLabel="rotating-lines-loading"
+        />
+      </div>
+    );
+  }
+
+  // Error Message
+  if (error) {
+    return <p className="text-red-500 text-xl text-center font-bold py-10 border-2 border-red-500 rounded-md m-5">{error}</p>;
+  }
+
+
   return (
     <div >
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <HomeCard key={post.index} post={post} />
-      ))}
+      ))} */}
+      {
+        posts.length === 0 ? (<p className="text-center text-red-600 text-xl font-semibold mt-6 border p-10 mx-2">There&apos;s nothing to show</p>
+        ) : <div>{posts.slice(0, visibleBlogs).map((post) => (
+          <HomeCard key={post.index} post={post} />
+        ))}</div >
+      }
     </div>
   );
 };
